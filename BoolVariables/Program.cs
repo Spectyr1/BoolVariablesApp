3e0701b5af4a5d458a4a5c3f9393e14b2d5a54c8
Danny Warren <danny@theforbiddenarts.com>
2022-03-22 22:43:13 -0400

    Final lesson code

--- BoolVariables/Program.cs
+++ BoolVariables/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// true or false
+
+bool isComplete = true;
+
+// isComplete = !isComplete;
+
+Console.WriteLine(!isComplete);
+Console.WriteLine(isComplete);
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 BoolVariablesApp
+
+
+discuss what is a bool
+
+discuss the NOT(!) character
